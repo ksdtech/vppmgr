a38be927f408ed94d097976cc93b8c4e37e087b2
Peter Zingg <peter@zingg.name>
2012-01-23 16:46:01 -0800

    app list

--- src/applications/vppmgr/controllers/products.py
+++ src/applications/vppmgr/controllers/products.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from import_vpp import read_products
-from update_vpp import update_products
+from update_vpp import select_apps, update_products
 
 def index():
-    return dict()
+    app_rows = select_apps(db)
+    return dict(apps=app_rows)
 
 def import_all():
     email, password = app_settings.email_login.split(':')
     products = read_products(email, password, app_settings.product_ss_name)
     updates = update_products(db, settings, products)
-    return dict(updates=updates)
+    request.flash = "Updated %d free and %d vpp apps" % (updates['free'], updates['vpp'])
+    redirect('index')
