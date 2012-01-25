d2170bcdb89051c78db9a6d4a795bd9818443696
Peter Zingg <peter@zingg.name>
2012-01-25 11:39:16 -0800

    case insensitive

--- src/applications/vppmgr/modules/ss_helper.py
+++ src/applications/vppmgr/modules/ss_helper.py
@@ -6,7 +6,7 @@ def nearest_app_match(apps, ss_name):
         app_name = None
         prev_name = None
         for i, product in enumerate(apps):
-            app_name = product.name
+            app_name = product.name_nocase
             if prev_name is not None and ss_name >= prev_name and ss_name < app_name:
                 return apps[i-1].id
             prev_name = app_name
@@ -21,7 +21,7 @@ def spreadsheet_app_select(spreadsheets, i, apps):
     select_id = 'sel_%d' % (spreadsheets[i].id)
     selected_app = spreadsheets[i].product
     if selected_app is None:
-        selected_app = nearest_app_match(apps, spreadsheets[i].spreadsheet_name)
+        selected_app = nearest_app_match(apps, spreadsheets[i].spreadsheet_name_nocase)
     s = '<select name="%s" id="%s">' % (select_id, select_id)
     for product in apps:
         s += '<option value="%s"' % (product.id)
