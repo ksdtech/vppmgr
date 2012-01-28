5fc949912c320ea2a619b3af5f49e71934997258
Peter Zingg <peter@zingg.name>
2012-01-28 09:33:34 -0800

    make us the default application

---
+++ src/routes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# default_application, used when the respective element is missing from the
+# (possibly rewritten) incoming URL
+#
+default_application = 'vppmgr'
